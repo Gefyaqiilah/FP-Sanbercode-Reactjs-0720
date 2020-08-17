d15deaff5e46b66effb2eb110893ff1169da685b
Gefyaqiilah <gefyaqiilah26@gmail.com>
2020-08-17 19:35:17 +0700

    solved page not found with adding _redirect file

---
+++ build/precache-manifest.38e1774cf17e7c9067621bca24e4a9ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d83d12866f1b09d7908737e99454ccf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad434724c4645922e63a",
+    "url": "/static/css/main.0530c902.chunk.css"
+  },
+  {
+    "revision": "705bf08ca41f5a11e27c",
+    "url": "/static/js/2.a1fc023d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a1fc023d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad434724c4645922e63a",
+    "url": "/static/js/main.8b7cb1db.chunk.js"
+  },
+  {
+    "revision": "d771402885d829c7a37d",
+    "url": "/static/js/runtime-main.16b2bd22.js"
+  }
+]);
